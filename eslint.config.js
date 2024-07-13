b062bbc58954d0e463d0cccfc5dd2d9430f1fceb
yuki-js <me@aoki.app>
2024-07-14 02:49:58 +0900

    add boilerplate

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// @ts-check
+
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  
+);
