c1d44185337d606f24294778fd181119beac7536
yuki-js <me@aoki.app>
2024-07-14 22:15:32 +0900

    fix example

--- packages/examples/index.ts
+++ packages/examples/index.ts
@@ -1,4 +1,4 @@
-import config, { load } from "@kintaman-co/nfigure";
+import config, { nfigure } from "@kintaman-co/nfigure";
 import typeboxValidator from "@kintaman-co/nfigure-typebox";
 import { Type } from "@sinclair/typebox";
 import assert from "node:assert/strict";
@@ -14,7 +14,7 @@ assert.deepStrictEqual(
   "config should be loaded and merged correctly",
 );
 
-const customConfig = load({});
+const customConfig = nfigure();
 
 assert.ok(customConfig, "customConfig should be loaded");
 assert.deepStrictEqual(
@@ -27,7 +27,7 @@ assert.deepStrictEqual(
   "customConfig should be loaded and merged correctly",
 );
 
-const customConfigWithValidator = load({
+const customConfigWithValidator = nfigure({
   validator: typeboxValidator(
     Type.Object({
       base: Type.String(),
@@ -51,17 +51,17 @@ assert.deepStrictEqual(
   },
   "customConfigWithValidator should be loaded and merged correctly",
 );
-
-const failureConfig = load({
-  validator: typeboxValidator(
-    Type.Object({
-      base: Type.Const("Never gonna give you up"),
-      overriden: Type.Const("Never gonna let you down"),
-      new: Type.Const("Never gonna run around and desert you"),
-      beingRemoved: Type.Null(),
-    }),
-  ),
-});
-
-// This should throw an error and crash the process
-assert.ok(!failureConfig, "failureConfig should not be loaded");
+try {
+  nfigure({
+    validator: typeboxValidator(
+      Type.Object({
+        base: Type.Const("Never gonna give you up"),
+        overriden: Type.Const("Never gonna let you down"),
+        new: Type.Const("Never gonna run around and desert you"),
+        beingRemoved: Type.Null(),
+      }),
+    ),
+  });
+} catch (_) {
+  assert.ok(true, "config validation failed");
+}
