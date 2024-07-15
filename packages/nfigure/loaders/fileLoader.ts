6b5bb04fcf15bee8bfdf4d98218c4f2763bdba8c
yuki-js <me@aoki.app>
2024-07-15 19:12:52 +0900

    use node namespace

--- packages/nfigure/loaders/fileLoader.ts
+++ packages/nfigure/loaders/fileLoader.ts
@@ -1,5 +1,5 @@
-import { resolve } from "path";
-import { Mode, readFileSync } from "fs";
+import { resolve } from "node:path";
+import { Mode, readFileSync } from "node:fs";
 import { CfgInternal } from "../cfgcfg";
 
 export function fileLoader() {
